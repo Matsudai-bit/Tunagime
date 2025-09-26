794d039ea2589ee0024af70945ff02785ed1f2b6
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2025-09-26 20:45:04 +0900

    ワールドオブジェクトの配置とプレファブ化

---
+++ Assets/DaikiData/Scripts/StageSelect/StageID.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ステージID
+/// </summary>
+public enum StageID
+{
+    NONE = -1,
+    STAGE_1 = 0,
+    STAGE_2 = 1,
+    STAGE_3 = 2,
+    STAGE_4 = 3,
+    STAGE_5 = 4,
+}
---
+++ Assets/DaikiData/Scripts/StageSelect/StageSelectCamera.cs
@@ -0,0 +1,55 @@
+﻿using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+
+/// <summary>
+/// ステージセレクトのカメラクラス
+/// </summary>
+public class StageSelectCamera : MonoBehaviour
+{
+    [SerializeField]
+    private StageTransformsForWorldObject m_stageTransformsForWorldObject; // ステージのTransformを格納するクラス
+
+    Transform m_startTransform;// スタート地点のTransform
+
+    private void Awake()
+    {
+        m_startTransform = transform;
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var stageTransforms = m_stageTransformsForWorldObject.StageTransforms;
+
+        var targetTransform = stageTransforms[StageID.STAGE_1];
+
+        MoveTarget(targetTransform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveTarget(Transform targetTransform)
+    {
+        // スクリーン座標取得  
+        var screenPos = new Vector2(Screen.width - Screen.width / 4, Screen.height / 2);
+        // ワールド座標に変換  
+        var worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+
+
+
+        var targetDirection = targetTransform.position - transform.position;
+        var velocity = (targetDirection.magnitude - m_stageTransformsForWorldObject.worldTransform.localScale.x) * targetDirection.normalized;
+
+        Quaternion.LookRotation(targetDirection.normalized, Vector3.up) ;
+
+        transform.DORotateQuaternion
+            (Quaternion.LookRotation(targetDirection.normalized, Vector3.up), 1.0f).SetEase(Ease.OutQuint);
+        transform.DOBlendableMoveBy(velocity, 1.5f).SetEase(Ease.OutQuint);
+    }
+}
