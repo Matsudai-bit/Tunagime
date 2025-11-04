66cfe1cf452bd992f477976dc1ca93563601e91e
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2025-11-04 16:20:25 +0900

    ロード非同期の作成

---
+++ Assets/DaikiData/Scripts/LoadingScene/LoadingSceneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// ローディングシーンのコントローラー
+/// </summary>
+public class LoadingSceneController : MonoBehaviour
+{
+    [SerializeField] private GameObject m_loadingUI;
+    [SerializeField] private Slider m_slider;
+
+    [SerializeField] public string sceneName ;
+
+    void Start()
+    {
+        // 次のシーン名を取得
+        sceneName = LoadingSceneRequest.GetInstance.NextSceneName;
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        m_loadingUI.SetActive(true);
+        StartCoroutine(LoadScene());
+    }
+    IEnumerator LoadScene()
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync("GameplayScene");
+        while (!async.isDone)
+        {
+            m_slider.value = async.progress;
+            yield return null;
+        }
+    }
+}
